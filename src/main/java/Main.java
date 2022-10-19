d9b4859c7cd0383a9071022ae218e1fe8b6e6688
Austin Foster <78121041+austindfoster@users.noreply.github.com>
2022-10-19 01:36:12 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-      int num = 0;
-      Calculator calc = new Calculator();
-        if (args[0] == "add") {
-          num = calc.add(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
-        } else if (args[0] == "subtract") {
-          num = calc.subtract(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
-        } else if (args[0] == "multiply") {
-          num = calc.multiply(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
-        } else if (args[0] == "divide") {
-          num = calc.divide(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
-        } else if (args[0] == "fibonacci") {
-          num = calc.fibonacciNumberFinder(Integer.parseInt(args[1]));
-        } else if (args[0] == "binary") {
-          num = calc.intToBinary(Integer.parseInt(args[1]));
+        boolean b = false;
+        int num = 0;
+        String bin = "";
+        Calculator calc = new Calculator();
+        if (Objects.equals(args[0], "add")) {
+            num = calc.add(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        } else if (Objects.equals(args[0], "subtract")) {
+            num = calc.subtract(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        } else if (Objects.equals(args[0], "multiply")) {
+            num = calc.multiply(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        } else if (Objects.equals(args[0], "divide")) {
+            num = calc.divide(Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        } else if (Objects.equals(args[0], "fibonacci")) {
+            num = calc.fibonacciNumberFinder(Integer.parseInt(args[1]));
+        } else if (Objects.equals(args[0], "binary")) {
+            bin = calc.intToBinaryNumber(Integer.parseInt(args[1]));
+            System.out.println(bin);
+            b = true;
         }
-      System.out.println(num);
+        if (!b) {
+            System.out.println(num);
+        }
     }
 }
